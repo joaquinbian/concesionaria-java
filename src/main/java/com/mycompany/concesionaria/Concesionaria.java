f674d9744aecfc86c3ccbd8196b008d81e960860
Joaquin Bianchi <joacobian21@gmail.com>
2024-02-18 23:01:32 -0300

    Primera pantalla

--- src/main/java/com/mycompany/concesionaria/Concesionaria.java
+++ src/main/java/com/mycompany/concesionaria/Concesionaria.java
@@ -1,11 +1,15 @@
 package com.mycompany.concesionaria;
 
-import com.mycompany.concesionaria.persistencia.ControladoraPersistencia;
+import com.mycompany.concesionaria.igu.Principal;
 
 public class Concesionaria {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        ControladoraPersistencia controladoraPersistencia = new ControladoraPersistencia();
+        
+        Principal app = new Principal();
+        
+        app.setVisible(true);
+        app.setLocationRelativeTo(null);
     }
 }
